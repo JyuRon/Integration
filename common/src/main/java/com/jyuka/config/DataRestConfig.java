83476088cf2547e382be9c58be1a04fcbc8be2cf
Jyuka <yoyksks3@gmail.com>
2023-05-25 18:41:13 +0900

    게시판 서비스 복습

--- common/src/main/java/com/jyuka/config/DataRestConfig.java
+++ common/src/main/java/com/jyuka/config/DataRestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 @Configuration
 public class DataRestConfig {
 
+    /** data rest 기본 설정은 id를 감추는 것인데,회원 계정에 한해서 `userId`가 노출되게끔 해줘야 함  */
     @Bean
     public RepositoryRestConfigurer repositoryRestConfigurer() {
         return RepositoryRestConfigurer.withConfig((config, cors) ->
